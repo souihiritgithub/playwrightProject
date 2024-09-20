3a77c0d41bb292fb8bb211d604475bcd8b996b09
souihiritgithub <souihi.islem2019@gmail.com>
2024-09-20 16:52:45 +0100

    second commit

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -435,3 +435,4 @@ async function checkTodosInLocalStorage(page: Page, title: string) {
     return JSON.parse(localStorage['react-todos']).map((todo: any) => todo.title).includes(t);
   }, title);
 }
+
