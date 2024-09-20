dc023d0af4b0ed0ec22e7efbe1b91c819e49fece
souihiritgithub <souihi.islem2019@gmail.com>
2024-09-20 17:52:23 +0100

    cucumber config

--- tests/support/hooks.ts
+++ tests/support/hooks.ts
@@ -1,17 +1,16 @@
-// tests/support/hooks.ts
-
 import { Before, After } from '@cucumber/cucumber';
-import { chromium, Browser, Page } from 'playwright';
+import { chromium, Browser, Page } from '@playwright/test';
 
 let browser: Browser;
 let page: Page;
 
 Before(async function () {
-  browser = await chromium.launch();
-  page = await browser.newPage();
-  this.page = page;
+  browser = await chromium.launch({ headless: false }); // Lancer le navigateur en mode visible
+  const context = await browser.newContext();
+  page = await context.newPage();
+  this.page = page; // Stocker la page dans le contexte de Cucumber
 });
 
 After(async function () {
-  await browser.close();
+  await browser.close(); // Fermer le navigateur après chaque scénario
 });
